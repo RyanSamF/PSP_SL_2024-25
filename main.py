e24c4d855b6d1fd1b8f499a702810fca15322192
RyanSamF <ryansamforbes@gmail.com>
2024-10-11 13:53:00 -0400

    improved accuracy to openrocket

--- main.py
+++ main.py
@@ -24,23 +24,24 @@ parachutes_data = data["parachutes"]
 
 thrusturl = files_data["thrusturl"]
 
-df = pandas.read_csv(thrusturl, index_col=None)
-time_thrust = np.array(df[df.columns[0]].tolist())
-thrust = np.array(df[df.columns[1]].tolist())
-thrust_array = np.stack([time_thrust, thrust], 1)
+#df = pandas.read_csv(thrusturl, index_col=None)
+#time_thrust = np.array(df[df.columns[0]].tolist())
+#thrust = np.array(df[df.columns[1]].tolist())
+#thrust_array = np.stack([time_thrust, thrust], 1)
+#thrust_array = "CSV_files/Wolf_thrust.csv"
 
 dragurl = files_data["dragurl"]
-df = pandas.read_csv(dragurl, index_col=None)
-time_drag = np.array(df[df.columns[0]].tolist())
-drag = np.array(df[df.columns[1]].tolist())
-drag_array = np.stack([time_drag, drag], 1)
+#df = pandas.read_csv(dragurl, index_col=None)
+#time_drag = np.array(df[df.columns[0]].tolist())
+#drag = np.array(df[df.columns[1]].tolist())
+#drag_array = np.stack([time_drag, drag], 1)
 
 
 #env.set_atmospheric_model(type ="wyoming_sounding", file = URL)
 #env.set_atmospheric_model(type = "Windy", file="")
 env_arr = []
 for wind_speed in speeds_ms:
-    env = Environment(latitude = 34.894616, longitude = -86.616947)
+    env = Environment(latitude = 20.6, longitude = -80.6)
     #URL = "http://weather.uwyo.edu/cgi-bin/sound   ing?region=naconf&TYPE=TEXT%3ALIST&YEAR=2024&MONTH=04&FROM=1300&TO=1312&STNM=72230"
     env.set_date((2024, 4, 13, 6))
     env.set_atmospheric_model(
@@ -60,14 +61,14 @@ airfoilLift = np.array(airfoilLift)
 
 L930 = GenericMotor(
     coordinate_system_orientation= "nozzle_to_combustion_chamber",
-    thrust_source = thrust_array,
+    thrust_source = thrusturl,
     dry_mass = (motor_data["net_mass"] - motor_data["prop_mass"]) * 0.4536, #lbs -> kg
     propellant_initial_mass = motor_data["prop_mass"] * 0.4536, #kg
-    center_of_dry_mass_position = motor_data["center_of_dry_mass"] / 39.37, #in -> m
+    #center_of_dry_mass_position = motor_data["center_of_dry_mass"] / 39.37, #in -> m
     #dry_inertia = (1.22 / 4.882, 1.22 / 4.882, 0.042 / 4.882),
     chamber_radius= motor_data["chamber_rad"] /39.37  ,#in to meters
     chamber_height = motor_data["chamber_height"] / 39.37, #in to meters
-    chamber_position = motor_data["chamber_pos"] / 39.37, #in to meters
+    chamber_position = motor_data["center_of_dry_mass"] / 39.37, #in to meters
     nozzle_radius = motor_data["nozzle_rad"] / 39.37, #in to meters
     burn_time = None
 )
@@ -76,8 +77,8 @@ wolf = Rocket(
     radius = rocket_data["radius"] / 39.37, #radius in -> meters
     mass = (rocket_data["mass"] / 2.2046), #mass lbs -> kg
     inertia = np.array(rocket_data["inertia"]) * 0.0421401101, #inertia lbs/ft^2 -> kg/m^2
-    power_off_drag = drag_array,
-    power_on_drag = drag_array,
+    power_off_drag = dragurl,
+    power_on_drag = dragurl,
     coordinate_system_orientation = "nose_to_tail",
     center_of_mass_without_motor = rocket_data["COM"] / 39.37 #in -> meters
 )
@@ -96,7 +97,7 @@ fin_set = wolf.add_trapezoidal_fins(
     position = fins_data["position"] / 39.37, #in -> meters
     span = fins_data["span"] / 39.37, #in -> meters
     sweep_length = 3.7 / 39.37,
-    airfoil = (airfoilLift, "degrees")
+    #airfoil = (airfoilLift, "degrees")
 )
 main = wolf.add_parachute(
     name = "main",
@@ -109,7 +110,7 @@ drogue = wolf.add_parachute(
     cd_s = parachutes_data["drogue_cd"] * (parachutes_data["drogue_diameter"] / 2 / 39.37) ** 2 * math.pi,
     trigger = "apogee"
 )
-
+#wolf.draw()
 final_vel = ["Final Velocity (ft/s)"]
 stability = ["Stability off rod (calibers)"]
 descent_time = ["Descent Time (seconds)"]
@@ -151,23 +152,21 @@ for i in range(0,5):
         ax2.set_ylim(bottom = ax2_ylims[1]*ax1_yratio)
     else:
         ax1.set_ylim(bottom = ax1_ylims[1]*ax2_yratio)
-    plt.text(testFlight.apogee_time + 0.1 ,100,'Apogee',rotation=90)
     plt.suptitle("Flight Parameters against Time",
                 fontweight = 'bold')
     plot_name = str(speeds[i])+" mph " + str(angles[i]) + " Degrees"
     plt.xlim((0, time[-1]))
     plt.title(plot_name)
     plt.grid()
-    plt.show()
-
+    #plt.show()
     final_vel.append(vel[-1])
-    stability.append(testFlight.stability_margin(testFlight.out_of_rail_time))
+    stability.append(testFlight.min_stability_margin)
     descent_time.append(time[-1] - testFlight.apogee_time)
     apogee.append((testFlight.apogee - env.elevation) * 3.28084)
     distance.append(descent_time[-1] * speeds_ms[i] * 3.28084)
     run_params.append(plot_name)
 
-end_results = [run_params, final_vel, stability, descent_time, apogee, distance]
+end_results = [run_params, final_vel, descent_time, apogee, distance] #stability removed
 print([i for i in end_results])
 
 import csv
